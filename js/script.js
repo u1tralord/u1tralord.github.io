71240afe8e39849280f40a8ad6f47518f8323ede
Jacob Poston <jacobpsemail@yahoo.com>
2015-04-16 10:35:01 -0400

    Finished contact form. Working on footer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
-console.log("TEST!");
-function test() {
+$( ".field" ).hover(
+  function() {
+      $(this).addClass("field-expanded");
+  },
+  function() {
+    	if($( this ).find( ".text-area" ).val() == ""){
+    	$(this).removeClass("field-expanded");
+      }
+  }
+)
 
-}
+$( ".field" ).click(function() {
+  $(this).addClass("field-expanded");
+});
+
+$( ".text-area" ).blur(
+  function() {
+    //$( this ).find( ".text-area" ).css( "background-color", "red" );
+    var value = $( this ).val();
+    if(value != "");
+  }
+);
